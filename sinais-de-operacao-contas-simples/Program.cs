36279ef8a514aafeee79d82651c42aa25af2b23b
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-14 12:09:02 -0300

    Add project files.

---
+++ sinais-de-operacao-contas-simples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escolha uma operação:");
+        Console.WriteLine("1 - Adição");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+
+        int operacao = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o primeiro número:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch (operacao)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                resultado = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Operação inválida.");
+                break;
+        }
+
+        Console.WriteLine("O resultado da operação é: " + resultado);
+
+        Console.ReadLine();
+    }
+}
